8752404b1d114a97e5c9482f3f7dfe0fbac537ff
Iggy <amargos.ignasi@gmail.com>
2017-03-08 10:18:45 +0100

    Gallery included

--- app.js
+++ app.js
@@ -15,7 +15,23 @@ var urlMaps = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.API_G
 console.log(urlMaps)
 
 app.get('/trip', (req, res) => {
-  res.render('trip', { urlMaps })
+  const photos = [
+    { url: '/img/1280x760/india_1_1280x760.jpeg',
+      des: 'Photo number 1' },
+    { url: '/img/1280x760/india_2_1280x760.jpeg',
+      des: 'Photo number 2' },
+    { url: '/img/1280x760/india_3_1280x760.jpeg',
+      des: 'Photo number 3' },
+    { url: '/img/1280x760/india_4_1280x760.jpeg',
+      des: 'Photo number 4' },
+    { url: '/img/1280x760/india_5_1280x760.jpeg',
+      des: 'Photo number 5' },
+    { url: '/img/1280x760/india_6_1280x760.jpeg',
+      des: 'Photo number 6' },
+    { url: '/img/1280x760/india_7_1280x760.jpeg',
+      des: 'Photo number 1' }
+  ]
+  res.render('trip', { urlMaps, photos })
 })
 
 // app.get('/home', (req, res) => {
