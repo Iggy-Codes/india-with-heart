145bd5c7fd3450bdfc645d87e7fae0f2034fee60
Iggy <amargos.ignasi@gmail.com>
2017-03-19 17:40:23 +0100

    Refactored api/img

--- server/routes/api/img/handlers/getAll.js
+++ server/routes/api/img/handlers/getAll.js
@@ -1,4 +1,3 @@
-// const fs = require('fs')
 const path = require('path')
 var read = require('fs-readdir-recursive')
 
@@ -6,13 +5,11 @@ module.exports = (req, res) => {
   try {
     let { folder } = req.params
     folder = !folder ? 'trip' : folder
-    const folderImg = path.join(__dirname, '../../../../public/img/' + folder)
+    const folderImg = path.join(__base, 'public/img/' + folder)// eslint-disable-line no-undef
     console.log(folderImg)
     const files = read(folderImg)
-    // let jsonFiles = {'imgs': JSON.stringify(files)}
     res.status(200).json({files})
   } catch (err) {
     res.status(500).json(err)
   }
 }
-// .map((element) => ({element}))
