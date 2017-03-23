1816e742521b8396d8b5badd3c1fbc28c9d011d0
Iggy <amargos.ignasi@gmail.com>
2017-03-23 09:39:07 +0100

    Not sending hmtl format in mail

--- server/routes/root/handlers/send-mail.js
+++ server/routes/root/handlers/send-mail.js
@@ -14,13 +14,13 @@ module.exports = (req, res) => {
     from: process.env.MAIL_GOOGLE_USER,
     to: process.env.MAIL_GOOGLE_USER,
     subject: 'Nuevo contacto web',
-    text: `Nuevo contacto\nNombre: ${name}\nEmail: ${email}\nTeléfono: ${phone}\nMensaje: ${message}`,
-    html: `<h2>Nuevo contacto WEB</h2>
-        <table>
-          <tr><th>Nombre:</th><th>Email:</th><th>Teléfono:</th></tr>
-          <tr><td><h3>${name}</h3></td><td><h3>${email}</td></h3><td><h3>${phone}</h3></td></tr>
-          <tr><td colspan="3"><h3>${message}</td></tr>
-        </table>`
+    text: `Nuevo contacto\nNombre: ${name}\nEmail: ${email}\nTeléfono: ${phone}\nMensaje: ${message}`
+    // html: `<h2>Nuevo contacto WEB</h2>
+    //     <table>
+    //       <tr><th>Nombre:</th><th>Email:</th><th>Teléfono:</th></tr>
+    //       <tr><td><h3>${name}</h3></td><td><h3>${email}</td></h3><td><h3>${phone}</h3></td></tr>
+    //       <tr><td colspan="3"><h3>${message}</td></tr>
+    //     </table>`
   }
   transporter.sendMail(mailOptions, (error, info) => {
     let result = true
