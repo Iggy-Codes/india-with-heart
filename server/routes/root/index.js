bf18f212b6b7d62b658e86d500a7c9a79ce7c2ec
Iggy <amargos.ignasi@gmail.com>
2017-03-23 04:41:25 +0100

    Contact form send working

--- server/routes/root/index.js
+++ server/routes/root/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const routerTrips = require('./handlers/trips')
 const routerDetailTrip = require('./handlers/detail-trip')
+const sendMail = require('./handlers/sendMail')
 
 router.get('/trip/:trip', (req, res, next) => {
   res.redirect('/trip/' + req.params.trip + '/_')
@@ -19,5 +20,6 @@ router.get('/contact', (req, res) => {
 
 router.get('/trip/:tripUri/:city', routerDetailTrip)
 router.get('/trips', routerTrips)
+router.post('/sendmail', sendMail)
 
 module.exports = router
