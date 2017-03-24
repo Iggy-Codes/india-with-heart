cae41b42b5a402a5a2b52c73c6c975a836a3302b
Iggy <amargos.ignasi@gmail.com>
2017-03-24 10:46:38 +0100

    Sorted selection alphabetically

--- server/routes/api/destination/handlers/getAll.js
+++ server/routes/api/destination/handlers/getAll.js
@@ -2,7 +2,9 @@ const path = require('path')
 const Destination = require(path.join(__base, 'models/Destination'))// eslint-disable-line no-undef
 
 module.exports = (req, res) => {
-  Destination.find()
+  Destination
+    .find()
+    .sort({ name: 1 })
     .then(tasks => res.json(tasks))
     // .then(tasks => res.json({tasks}))
     .catch(err => { throw err })
--- server/routes/api/trip/handlers/getAll.js
+++ server/routes/api/trip/handlers/getAll.js
@@ -2,7 +2,9 @@ const path = require('path')
 const Trip = require(path.join(__base, 'models/Trip'))// eslint-disable-line no-undef
 
 module.exports = (req, res) => {
-  Trip.find()
+  Trip
+    .find()
+    .sort({ title: 1 })
     .populate('destinations')
     .then(trip => res.json(trip))
     .catch(err => { throw err })
